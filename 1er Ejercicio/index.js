4e8e68aa9a277aff501e116f61cedb7031d9f037
patocarbone <88810457+patocarbone@users.noreply.github.com>
2022-06-23 19:50:51 -0300

    Add files via upload

---
+++ 1er Ejercicio/index.js
@@ -0,0 +1,11 @@
+// Declaro nuevo set
+
+const set = new Set (['Patricio', 'Valeria', 'Roberto'])
+console.log(set)
+
+// Anado mi nombre
+set.add('Patricio')
+
+// Anado Javascript
+set.add('Javascript')
+console.log(set)
